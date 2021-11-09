7c0a0d7651b12ab18528875b74f5ec922de3e9fa
Lee Siang Yin <siangyin916@gmail.com>
2021-11-10 03:02:21 +0800

    Updated Diary.jsx date input

--- src/components/Diary.jsx
+++ src/components/Diary.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { preText } from "./data";
 
 function Diary() {
 	const randomText = preText[Math.floor(Math.random() * preText.length)];
+	const today = new Date();
+	const [date, setDate] = useState(
+		today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate()
+	);
+
+	function handleChange(e) {
+		const label = e.target;
+		const value = e.target.value;
+		console.log(label, value);
+		setDate(value);
+	}
 
 	return (
 		<form>
 			<label htmlFor="date">Date: </label>
-			<input type="date" id="date" name="date"></input>
+			<input
+				type="date"
+				id="date"
+				name="date"
+				value={date}
+				onChange={handleChange}
+			></input>
 			<br />
 			<textarea
 				id="story"
