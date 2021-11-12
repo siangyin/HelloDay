2468dd15b435cd4357f2335274250856e4955552
Lee Siang Yin <siangyin916@gmail.com>
2021-11-13 04:19:40 +0800

    added dynamic path to diary/date with Link (prob: link is not sync with edit function) will show blank page if edit button is not clicked

--- src/components/AllDiaries.jsx
+++ src/components/AllDiaries.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const AllDiaries = ({ diaries, handleDelete, handleEdit }) => {
 	return (
@@ -7,24 +8,26 @@ const AllDiaries = ({ diaries, handleDelete, handleEdit }) => {
 			{diaries.map((day) => {
 				return (
 					<div key={day.date} className="entries-list">
-						{day.mood.icon} <strong>{day.title}</strong>
-						{day.tag.map((tag, i) => (
-							<em key={i}>{tag} </em>
-						))}
-						<button
-							onClick={() => {
-								handleEdit(day.date);
-							}}
-						>
-							Edit
-						</button>
-						<button
-							onClick={() => {
-								handleDelete(day.date);
-							}}
-						>
-							delete
-						</button>
+						<Link to={`/diary/${day.date}`}>
+							{day.mood.icon} <strong>{day.title}</strong>
+							{day.tag.map((tag, i) => (
+								<em key={i}>{tag} </em>
+							))}
+							<button
+								onClick={() => {
+									handleEdit(day.date);
+								}}
+							>
+								Edit
+							</button>
+							<button
+								onClick={() => {
+									handleDelete(day.date);
+								}}
+							>
+								delete
+							</button>
+						</Link>
 					</div>
 				);
 			})}
