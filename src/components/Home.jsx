46d6599572d0456b6d448782044851940b19f8f3
Lee Siang Yin <siangyin916@gmail.com>
2021-11-13 12:19:41 +0800

    import { getAuth, createUserWithEmailAndPassword } from "firebase/auth" to handle user signup in homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { useRef } from "react";
 import image from "../images/writingdiary.svg";
+import { signUp } from "../firebase/firebase-config";
 
 const Home = () => {
+	const emailRef = useRef();
+	const passwordRef = useRef();
+
+	async function handleSignUp() {
+		await signUp(emailRef.current.value, passwordRef.current.value);
+	}
+
 	return (
 		<div className="home">
 			<h2>Homepage component</h2>
@@ -9,23 +17,30 @@ const Home = () => {
 			<div className="user-login">
 				<img src={image} style={{ width: "50vw" }} alt="writingdiary"></img>
 				<form className="login-form-form">
-					<h4 className="login-form-label">Username: </h4>
+					<h4 className="login-form-label">Email: </h4>
 					<input
 						className="login-form-input"
-						type="text"
-						name="username"
+						ref={emailRef}
+						type="email"
+						name="email"
 					></input>
 					<br />
 
 					<h4 className="login-form-label">Password: </h4>
 					<input
 						className="login-form-input"
+						ref={passwordRef}
 						type="password"
 						name="password"
 					></input>
 					<br />
 
-					<button type="submit">Add</button>
+					<button type="button" onClick={handleSignUp}>
+						Log in
+					</button>
+					<button type="button" onClick={handleSignUp}>
+						Sign up
+					</button>
 				</form>
 			</div>
 		</div>
